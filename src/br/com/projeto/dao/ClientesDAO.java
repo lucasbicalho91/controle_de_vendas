ab67c80b49b6be4cac1027addf27a9b84af30551
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-12-04 20:20:02 -0300

    Criado o método para listar Clientes

--- src/br/com/projeto/dao/ClientesDAO.java
+++ src/br/com/projeto/dao/ClientesDAO.java
@@ -10,6 +10,9 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.ResultSet;
 
 /**
  *
@@ -66,6 +69,50 @@ public class ClientesDAO {
     
     // Método excluirCliente
     public void excluirCliente() {
+        
+    }
+    
+    // Método listarClientes
+    public List<Clientes> listarClientes() {
+        try {
+            
+            // primeiro passo: Criar a lista
+            List<Clientes> lista = new ArrayList<>();
+            
+            // segundo passo: Criar o comando SQL, organizar e executar o comando
+            String sql = "select * from tb_clientes";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()) {
+                Clientes obj = new Clientes();
+                
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("uf"));
+                
+                lista.add(obj);
+                
+            }
+            
+            return lista;
+            
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+            return null;
+        }
         
     }
     
